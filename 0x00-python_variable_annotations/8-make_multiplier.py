a67a3018afb923c2a7ad6e6396a6d8f0f02a4140
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-19 22:30:16 +0000

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task 8 """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """using Callable from typing"""
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    return multiplier_function
