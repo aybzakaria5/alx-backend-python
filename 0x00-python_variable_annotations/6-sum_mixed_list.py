7183a6054163c28162aa9164e5622104b83ef956
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-19 10:42:57 +0000

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""mixed"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+    """union and list from typing"""
+    return sum(mxd_list)
