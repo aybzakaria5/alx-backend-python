37944af9fb552bb24d3070b4a3216a698ce121e9
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-02 15:39:50 +0000

    fixing pycodestyleé

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
-"""
-     an asynchronous coroutine
-     that takes in an integer argument
-     (max_delay, with a default value of 10)
-    """
-
+"""docs for task 1"""
 from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all the delays (float values).
-in ascending order without using sort() because of concurrency.
-    """
+    """docs for task 1"""
     sorted_tasks = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     completed = asyncio.as_completed(tasks)
     for task in completed:
         result = await task
         sorted_tasks.append(result)
-    return sorted_tasks # list of delay of tasks sorte as completed
+    return sorted_tasks
